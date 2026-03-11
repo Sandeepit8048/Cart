39d94b0c1f54b6dcdf717fd1dc3ebb964a0e4130
sandeepit8048 <rkysandeepyadav@gmail.com>
2026-03-11 09:57:17 +0530

    update nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,31 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { addcart,addcount } from "../store/Slice";
 
 function Navbar() {
-   const selector = useSelector((state)=>state.cart.items)
-  return (
-    <nav className="bg-green-600 text-white sm:px-6 sm:py-4 sm:flex sm:justify-between">
-      <h1 className="font-bold text-lg">EcoCheckout</h1>
+  const selector = useSelector((state) => state.cart.items);
 
-      <div className="sm:flex sm:gap-6 ">
-        <Link to="/Cart" className="hover:text-gray-200">Cart:{selector.length}</Link>
-        <Link to="/shipping" className="hover:text-gray-200">Shipping</Link>
-        <Link to="/payment" className="hover:text-gray-200">Payment</Link>
+  return (
+    <nav className="bg-green-600 text-white px-4 py-3">
+      <div className="max-w-6xl mx-auto flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3">
+
+        <h1 className="font-bold text-xl text-center sm:text-left">
+          EcoCheckout
+        </h1>
+
+        <div className="flex justify-center gap-6 text-sm sm:text-base">
+          <Link to="/cart" className="hover:text-gray-200">
+            Cart ({selector.length})
+          </Link>
+
+          <Link to="/shipping" className="hover:text-gray-200">
+            Shipping
+          </Link>
+
+          <Link to="/payment" className="hover:text-gray-200">
+            Payment
+          </Link>
+        </div>
+
       </div>
     </nav>
   );
